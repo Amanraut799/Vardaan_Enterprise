83a31249410decb3ec94439e0fc5b82ac52f2749
Aman Raut <your-email@example.com>
2026-06-03 00:34:04 +0530

    update

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -15,14 +15,14 @@ router.post("/", async (req, res) => {
     console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
     const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
 
     await transporter.verify();
 
