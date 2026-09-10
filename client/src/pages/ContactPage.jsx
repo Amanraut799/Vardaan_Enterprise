947808b031e3d23ced19c443fc8ae6323d6ba3e2
Amanraut799 <Amanraut799@gmail.com>
2026-09-10 19:33:12 +0530

    update recovery pdf

--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -179,7 +179,7 @@ function ContactPage() {
 
                 <div className="brochure-buttons">
                   <a
-                    href="/SMART Recovery1.pdf"
+                    href="/SMART Recovery.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
@@ -189,7 +189,7 @@ function ContactPage() {
                   </a>
 
                   <a
-                    href="/SMART Recovery1.pdf"
+                    href="/SMART Recovery.pdf"
                     download
                   >
                     <button className="download-brochure-btn">
